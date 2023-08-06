2997e31612bcbdf2ed99f4ca750157a2f59e51c3
tyltr <tylitianrui@126.com>
2023-08-06 15:03:37 +0800

    Dynamic Generator

---
+++ internal/preprocessing/dynamic.go
@@ -0,0 +1,44 @@
+package preprocessing
+
+import (
+	"sync"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+var DefaultDynamicGenerator *Dynamic
+
+func init() {
+	DefaultDynamicGenerator = NewDynamicGenerator(4)
+	DefaultDynamicGenerator.Register("gen_uuid()", GenUUID)
+}
+
+type DynamicGenerator func() string
+
+type Dynamic struct {
+	generators map[string]DynamicGenerator
+	mu         sync.Mutex
+}
+
+func NewDynamicGenerator(size int) *Dynamic {
+	return &Dynamic{
+		generators: make(map[string]DynamicGenerator, size),
+	}
+}
+
+func (d *Dynamic) Register(name string, gen DynamicGenerator) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.generators[name] = gen
+}
+func (d *Dynamic) GetDynamicGenerator(name string) (DynamicGenerator, bool) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	generator, exist := d.generators[name]
+	return generator, exist
+}
+
+func GenUUID() string {
+	u4 := uuid.NewV4()
+	return u4.String()
+}
---
+++ internal/preprocessing/dynamic_test.go
@@ -0,0 +1,18 @@
+package preprocessing
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDynamic(t *testing.T) {
+	a := assert.New(t)
+	gen, exist := DefaultDynamicGenerator.GetDynamicGenerator("uuid")
+	a.Equal(exist, false)
+	a.Nil(gen)
+
+	gen, exist = DefaultDynamicGenerator.GetDynamicGenerator("gen_uuid()")
+	a.Equal(exist, true)
+	a.NotNil(gen)
+}
