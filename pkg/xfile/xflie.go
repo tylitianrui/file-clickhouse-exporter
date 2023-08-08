0b27ac071848267b36622fad53943962929c03eb
tyltr <tylitianrui@126.com>
2023-08-08 17:23:04 +0800

    readline

--- pkg/xfile/xflie.go
+++ pkg/xfile/xflie.go
@@ -2,11 +2,36 @@ package xfile
 
 import (
 	"bufio"
+	"context"
 	"os"
 )
 
+const BuffSize = 1 << 4
+
+type XReader interface {
+	ReadLines(ctx context.Context) (chan []byte, chan error)
+}
+
+type FileLineGetter interface {
+	Line() []byte
+	Error() error
+}
+type FileLine struct {
+	line []byte
+	err  error
+}
+
+func (fl *FileLine) Line() []byte {
+	return fl.line
+}
+
+func (fl *FileLine) Error() error {
+	return fl.err
+}
+
 type FileReader struct {
-	reader *bufio.Reader
+	reader    *bufio.Reader
+	fileLines chan FileLineGetter
 }
 
 func NewFileReader(filename string) (*FileReader, error) {
@@ -17,15 +42,30 @@ func NewFileReader(filename string) (*FileReader, error) {
 
 	bufioReader := bufio.NewReader(fd)
 	fileReader := &FileReader{
-		reader: bufioReader,
+		reader:    bufioReader,
+		fileLines: make(chan FileLineGetter, BuffSize),
 	}
 	return fileReader, nil
 }
 
-func (fr *FileReader) ReadLine() ([]byte, error) {
-	return fr.reader.ReadBytes('\n')
+func (fr *FileReader) readLines(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			close(fr.fileLines)
+			return
+		default:
+			b, err := fr.reader.ReadBytes('\n')
+			line := &FileLine{
+				line: b,
+				err:  err,
+			}
+			fr.fileLines <- line
+		}
+	}
 }
 
-func (fr *FileReader) Reader() *bufio.Reader {
-	return fr.reader
+func (fr *FileReader) ReadLines(ctx context.Context) chan FileLineGetter {
+	go fr.readLines(ctx)
+	return fr.fileLines
 }
--- pkg/xfile/xflie_test.go
+++ pkg/xfile/xflie_test.go
@@ -1,6 +1,7 @@
 package xfile
 
 import (
+	"context"
 	"io"
 	"testing"
 
@@ -12,7 +13,7 @@ var (
 	txtb     = "world\n"
 	txtc     = "Delicate, lustrous, and soft to the touch. The ethereal fabric that we now call silk has threaded its way throughout China's history.One cannot be certain of its origin, but the humble ancient Chinese people credited their own wisdom to Leizu, wife of the Yellow Emperor (the legendary ancestor of all Chinese people), as the inventor of sericulture.\n"
 	txtd     = "Like almost all genres of art on the vast land of China, the style and texture of silk are also multifarious. Hangluo satin from Hangzhou, Zhejiang province, is renowned for its airy and sheer texture, while Yunjin brocade from Nanjing, Jiangsu province, a luxurious fabric often used for royal garments, represents China's silk weaving technique at its prime.Yunjin brocade is best made by hand on giant looms, in a complex procedure that comprises more than a hundred steps. Even the most skilled artisans can only weave a few centimeters a day. Time, patience and deftness all play imperative roles to its heavenly beauty, or as its name suggests, its cloud-like splendor.In the Western Han Dynasty (206 BC-AD 24), with diplomat and explorer Zhang Qian opening up the routes to the western regions, silk graced countries in Central Asia, later extending its reach to other parts of Eurasia and beyond. Fittingly, its name marked China's major international trade routes, the ancient Silk Road and Maritime Silk Road.In the hands of Chinese artists, the thinnest threads can weave pictures of immense possibilities, and the softest material can traverse thousands of years. As one of the many marvels of ancient China, silk is not merely a type of textile. It is a cultural icon, and an embodiment of elegance and grace.\n"
-	caselist = []string{txta, txtb, txtc, txtd, txtc, txtd, txtc, txtd, txtc, txtd, txtc, txtd}
+	caselist = []string{txta, txtb, txtc, txtd, txtc, txtd, txtc, txtd, txtc, txtd, txtc, txtd, ""}
 )
 
 func TestFileReader_ReadLine(t *testing.T) {
@@ -20,17 +21,22 @@ func TestFileReader_ReadLine(t *testing.T) {
 	filename := "../../test/test_read_line"
 	reader, err := NewFileReader(filename)
 	a.NoError(err)
-	var idx = 0
-	for {
-		b, err := reader.ReadLine()
-		if err != nil {
-			a.EqualError(err, io.EOF.Error())
-			break
+	ctx, cancel := context.WithCancel(context.Background())
+	content := reader.ReadLines(ctx)
+
+	var i int
+	for c := range content {
+		if i <= 12 {
+			a.Equal(caselist[i], string(c.Line()))
+			if i == 12 {
+				a.Equal(io.EOF, c.Error())
+			} else {
+				a.NoError(c.Error())
+			}
 		}
-		a.NoError(err)
-		a.Equal(caselist[idx], string(b))
-		idx++
-
+		if i > 11 {
+			cancel()
+		}
+		i++
 	}
-
 }
