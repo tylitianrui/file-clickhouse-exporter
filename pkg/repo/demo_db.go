278f9a7905ae7149cc80c83ce894e73d810fd103
tyltr <tylitianrui@126.com>
2023-08-11 09:52:51 +0800

    msg

--- pkg/repo/demo_db.go
+++ pkg/repo/demo_db.go
@@ -1,6 +1,7 @@
 package repo
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 )
@@ -16,5 +17,14 @@ func NewDemoRepo(config ClickhouseRepoConfig) (*DemoRepo, error) {
 
 func (cr *DemoRepo) BatchInsert(ctx context.Context, table string, columns []string, vals [][]interface{}, async bool) error {
 	fmt.Println("table:", table, " BatchInsert ok ", len(vals))
+
+	for _, val := range vals {
+		var columnVal bytes.Buffer
+		for _, v := range val {
+			vstr := fmt.Sprintf("%v  ", v)
+			columnVal.WriteString(vstr)
+		}
+		fmt.Println(columnVal.String())
+	}
 	return nil
 }
